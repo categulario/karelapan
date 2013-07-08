0da71c601fd9e99984672d9c6e6875ef01ce2745
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-07-08 09:28:53 -0500

    bug en la función

--- src/funcion_actualiza_puntaje.sql
+++ src/funcion_actualiza_puntaje.sql
@@ -1,4 +1,4 @@
-﻿CREATE OR REPLACE FUNCTION actualiza_puntaje_concurso(id_usuario integer, id_concurso integer)
+﻿CREATE OR REPLACE FUNCTION actualiza_puntaje(id_usuario integer)
   RETURNS void AS
 $BODY$
 	DECLARE
@@ -7,15 +7,15 @@ $BODY$
 		parcial INTEGER;
 	BEGIN
 		suma := 0;
-		FOR problema IN SELECT DISTINCT problema_id FROM evaluador_envio WHERE usuario_id = id_usuario AND concurso_id = id_concurso
+		FOR problema IN SELECT DISTINCT problema_id FROM evaluador_envio WHERE usuario_id = id_usuario AND concurso_id isnull
 		LOOP
-			SELECT MAX(puntaje) INTO parcial FROM evaluador_envio WHERE problema_id=problema.problema_id AND usuario_id=id_usuario AND concurso_id = id_concurso;
+			SELECT MAX(puntaje) INTO parcial FROM evaluador_envio WHERE problema_id=problema.problema_id AND usuario_id=id_usuario AND concurso_id isnull;
 			suma = suma + parcial;
 		END LOOP;
-		UPDATE evaluador_participacion SET puntaje=suma WHERE usuario_id=id_usuario AND concurso_id=id_concurso;
+		UPDATE usuarios_usuario SET puntaje=suma WHERE id=id_usuario;
 	END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION actualiza_puntaje_concurso(integer, integer)
+ALTER FUNCTION actualiza_puntaje(integer)
   OWNER TO covi;

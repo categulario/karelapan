45d4f15a89559ebf4b55b5ea9766506ee5809394
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-07-14 00:17:38 -0500

    configuración más completa de gunicorn

--- gconfig.py
+++ gconfig.py
@@ -1,4 +1,12 @@
+# -*- coding:utf-8 -*-
 import multiprocessing
 
-bind = "192.168.1.72:8000"
+bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
+preload_app = False #Carga el código de la aplicación antes de iniciar los trabajadores
+# Disminuye el uso de RAM pero hace más lento el reinicio del servidor
+daemon = False
+pidfile = '/home/abraham/Desarrollo/django/KarelapanDjango/gunicorn.pid'
+accesslog = '/home/abraham/Desarrollo/django/KarelapanDjango/access.log'
+errorlog = '/home/abraham/Desarrollo/django/KarelapanDjango/error.log'
+loglevel = 'info'

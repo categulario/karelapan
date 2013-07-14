cff37bbe8194e1ab5c1d30722b551622695ababc
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-07-14 10:03:43 -0500

    más facil configuración de gunicorn

--- gconfig.py
+++ gconfig.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
 import multiprocessing
+import os
 
 bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 preload_app = False #Carga el código de la aplicación antes de iniciar los trabajadores
 # Disminuye el uso de RAM pero hace más lento el reinicio del servidor
 daemon = False
-pidfile = '/home/abraham/Desarrollo/django/KarelapanDjango/gunicorn.pid'
-accesslog = '/home/abraham/Desarrollo/django/KarelapanDjango/access.log'
-errorlog = '/home/abraham/Desarrollo/django/KarelapanDjango/error.log'
+pidfile = os.path.normpath(os.path.join(os.path.dirname(__file__),'gunicorn.pid'))
+accesslog = os.path.normpath(os.path.join(os.path.dirname(__file__),'access.log'))
+errorlog = os.path.normpath(os.path.join(os.path.dirname(__file__),'error.log'))
 loglevel = 'info'

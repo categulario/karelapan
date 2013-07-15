351a730906c31730bc40015a5dbd72bb6420245b
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-07-15 11:23:03 -0500

    configuraciones de producción

--- karelapan/settings.py
+++ karelapan/settings.py
@@ -55,22 +55,22 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'../media/'))
+MEDIA_ROOT = '/var/www/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://192.168.1.83/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'../static/'))
+STATIC_ROOT = '/var/www/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://192.168.1.83/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (

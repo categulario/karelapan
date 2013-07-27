8d87ab16ef0746e1f03adaa7c7bda7c66f4b3460
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-07-26 22:54:08 -0500

    fondo animado

--- assets/js/default.js
+++ assets/js/default.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     $('.preventpropagation').click(function(event){
         event.stopPropagation();
     });
+    $("#bg-anim").pan({fps: 30, speed: 1, dir: 'left'});
+    $("#bg-anim").find('.span12').pan({fps: 30, speed: 2, dir: 'left'});
 });

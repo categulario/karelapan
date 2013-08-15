bca431876ac2b2412a4aa56a64cf4129a601634e
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-08-14 23:55:21 -0500

    html fields para avisos, noticias y faqs

---
+++ apps/sitio/migrations/0003_auto__chg_field_aviso_contenido__chg_field_preguntafrecuente_respuesta.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Aviso.contenido'
+        db.alter_column(u'sitio_aviso', 'contenido', self.gf('tinymce.models.HTMLField')())
+
+        # Changing field 'PreguntaFrecuente.respuesta'
+        db.alter_column(u'sitio_preguntafrecuente', 'respuesta', self.gf('tinymce.models.HTMLField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'Aviso.contenido'
+        db.alter_column(u'sitio_aviso', 'contenido', self.gf('django.db.models.fields.TextField')())
+
+        # Changing field 'PreguntaFrecuente.respuesta'
+        db.alter_column(u'sitio_preguntafrecuente', 'respuesta', self.gf('django.db.models.fields.TextField')())
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'sitio.aviso': {
+            'Meta': {'object_name': 'Aviso'},
+            'caducidad': ('django.db.models.fields.DateTimeField', [], {}),
+            'contenido': ('tinymce.models.HTMLField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mostrado': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'tipo': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        u'sitio.noticia': {
+            'Meta': {'ordering': "['-pegajoso', '-fecha']", 'object_name': 'Noticia'},
+            'autor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'contenido': ('tinymce.models.HTMLField', [], {}),
+            'fecha': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pegajoso': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '140'})
+        },
+        u'sitio.preguntafrecuente': {
+            'Meta': {'ordering': "['ordenacion']", 'object_name': 'PreguntaFrecuente'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mostrado': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'ordenacion': ('django.db.models.fields.IntegerField', [], {}),
+            'pregunta': ('django.db.models.fields.CharField', [], {'max_length': '254'}),
+            'respuesta': ('tinymce.models.HTMLField', [], {})
+        }
+    }
+
+    complete_apps = ['sitio']
--- apps/sitio/models.py
+++ apps/sitio/models.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 from django.db import models
 from django.utils import timezone
+from tinymce.models import HTMLField
 
 class Noticia(models.Model):
     titulo      = models.CharField(max_length=140)
-    contenido   = models.TextField()
+    contenido   = HTMLField()
     autor       = models.ForeignKey('usuarios.Usuario')
     fecha       = models.DateTimeField(auto_now_add=True)
     pegajoso    = models.BooleanField(default=False)
@@ -16,7 +17,7 @@ class Noticia(models.Model):
         ordering = ['-pegajoso', '-fecha']
 
 class Aviso(models.Model):
-    contenido   = models.TextField()
+    contenido   = HTMLField()
     tipo        = models.CharField(max_length=20, choices=(
         ('success', 'Bien (verde)'),
         ('info', 'Información (azul)'),
@@ -35,7 +36,7 @@ class Aviso(models.Model):
 
 class PreguntaFrecuente(models.Model):
     pregunta        = models.CharField(max_length=254)
-    respuesta       = models.TextField()
+    respuesta       = HTMLField()
     ordenacion      = models.IntegerField(choices=((i, str(i)) for i in xrange(1, 101)))
     mostrado        = models.BooleanField(default=True)
 

cf7ab480608b364a7a78feade16fc8efb6b50884
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-09-20 09:33:00 -0500

    gconfig

--- gconfig.py
+++ gconfig.py
@@ -6,7 +6,7 @@ bind = "192.168.1.72:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 preload_app = False #Carga el código de la aplicación antes de iniciar los trabajadores
 # Disminuye el uso de RAM pero hace más lento el reinicio del servidor
-daemon = False
+daemon = True
 pidfile = os.path.normpath(os.path.join(os.path.dirname(__file__),'gunicorn.pid'))
 accesslog = os.path.normpath(os.path.join(os.path.dirname(__file__),'access.log'))
 errorlog = os.path.normpath(os.path.join(os.path.dirname(__file__),'error.log'))

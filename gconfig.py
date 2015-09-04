889a40e7cb05d7e1e348b1e4a64dfb4e29fd0eef
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-09-03 23:53:37 -0500

    unified gconfig file

--- gconfig.py
+++ gconfig.py
@@ -2,12 +2,13 @@
 import multiprocessing
 import os
 
-bind = "127.0.0.1:8000"
+_debug = os.environ.get('DJANGO_SETTINGS_MODULE', 'karelapan.settings').endswith('dev')
+
+bind = "/tmp/gunicorn.sock" # "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-preload_app = True #Carga el código de la aplicación antes de iniciar los trabajadores
-# Disminuye el uso de RAM pero hace más lento el reinicio del servidor
-daemon = True
-pidfile = os.path.normpath(os.path.join(os.path.dirname(__file__),'gunicorn.pid'))
-accesslog = os.path.normpath(os.path.join(os.path.dirname(__file__),'access.log'))
-errorlog = os.path.normpath(os.path.join(os.path.dirname(__file__),'error.log'))
+preload_app = True
+daemon = not _debug
+pidfile = os.path.normpath(os.path.join(os.path.dirname(__file__), 'gunicorn.pid'))
+accesslog = os.path.normpath(os.path.join(os.path.dirname(__file__), 'access.log'))
+errorlog = os.path.normpath(os.path.join(os.path.dirname(__file__), 'error.log'))
 loglevel = 'info'

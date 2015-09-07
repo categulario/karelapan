e63a3f6c4489c8b2034eca027b21cc1d1d3e851f
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-09-06 23:01:30 -0500

    set paths for production environment

--- karelapan/settings.py
+++ karelapan/settings.py
@@ -53,27 +53,27 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/covi/temp/staticfiles/media/'
+MEDIA_ROOT = '/var/www/sites/karelapan_static/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://static.covi.org.mx/media/'
+MEDIA_URL = 'http://karelapan.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/covi/temp/staticfiles/static/'
+STATIC_ROOT = '/var/www/sites/karelapan_static/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = 'http://static.covi.org.mx/static/'
+STATIC_URL = 'http://karelapan.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    os.path.normpath(os.path.join(os.path.dirname(__file__),'../assets')),
+    os.path.join(PROJECT_PATH, 'assets'),
 )
 
 # List of finder classes that know how to find static files in
@@ -136,11 +136,6 @@ INSTALLED_APPS = (
     'mptt',
     'sekizai',
     'sorl.thumbnail',
-    'wiki',
-    'wiki.plugins.attachments',
-    'wiki.plugins.notifications',
-    'wiki.plugins.images',
-    'wiki.plugins.macros',
     'apps.bootstrapy',
     'apps.karelecatl',
     'apps.libro',
